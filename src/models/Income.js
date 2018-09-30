67f20a8a942995451a380c475317ed535a34ac10
Joakim Rosenstam <joakim.rosenstam@gmail.com>
2018-09-30 20:40:04 +0200

    moved create/update/delete logic to base class.

--- src/models/Income.js
+++ src/models/Income.js
@@ -1,61 +1,30 @@
 'use strict';
 
 import { FINANCIAL_TYPES, FinancialBase } from './FinancialBase';
-import api from '../api';
-import store from '../store';
 
 export class Income extends FinancialBase {
-	constructor (id, income, personId, title) {
-		super(FINANCIAL_TYPES.INCOME, 'income');
-		this.personId = personId;
-		this.title = title;
-		this.income = income;
-		this.id = id;
-	}
+    constructor (id, income, personId, title) {
+        super(Income.type());
 
-	static type () {
-		return FINANCIAL_TYPES.INCOME;
-	}
+        this.personId = personId;
+        this.title = title;
+        this.income = income;
+        this.id = id;
+    }
 
-	static url () {
-		return 'income';
-	}
+    static type () {
+        return FINANCIAL_TYPES.INCOME;
+    }
 
-	static create (income, personId, title) {
-		return new Promise((resolve) => {
-			api.add(Income.url(), {
-				personId: personId,
-				title: title,
-				income: income,
-			}).then(response => {
-				store.dispatch('addFinancialData', { type: Income.type(), data: response });
-				resolve();
-			});
-		});
-	}
+    static url () {
+        return 'income';
+    }
 
-	update (income) {
-		return new Promise((resolve) => {
-			api.update(Income.url(), {
-				id: this.id,
-				personId: this.personId,
-				title: this.title,
-				income: income,
-			}).then(response => {
-				store.dispatch('updateFinancialData', { instance: this, data: response });
-				resolve();
-			});
-		});
-	}
+    url () {
+        return 'income';
+    }
 
-	remove () {
-		return new Promise((resolve) => {
-			api.remove(Income.url(), {
-				id: this.id,
-			}).then(response => {
-				store.dispatch('deleteFinancialData', { instance: this, data: response });
-				resolve();
-			});
-		});
-	}
+    static create (data) {
+        return FinancialBase.create(this, data);
+    }
 }
